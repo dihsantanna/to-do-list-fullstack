eb4be218aa7d55665f464c92548374cc87289fd9
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-11-17 18:30:43 -0300

    feat: implements User entitie and your tests

---
+++ app/backend/src/app/entities/User/index.ts
@@ -0,0 +1,32 @@
+import { v4 as uuid } from 'uuid';
+
+export interface UserType {
+  id?: string
+  name: string
+  email: string
+  password: string
+  createdAt?: Date
+  updatedAt?: Date | null
+}
+
+export class User implements UserType {
+  private readonly _id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date | null;
+
+  constructor (props: UserType) {
+    this._id = props.id ?? uuid();
+    this.name = props.name;
+    this.email = props.email;
+    this.password = props.password;
+    this.createdAt = props.createdAt ?? new Date();
+    this.updatedAt = props.updatedAt ?? null;
+  }
+
+  get id (): string {
+    return this._id;
+  }
+}
