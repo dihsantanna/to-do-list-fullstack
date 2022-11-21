1cb75b67de9c3a649201e852206f9817477bce66
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-11-21 00:57:39 -0300

    feat: creates User controllers.

---
+++ app/backend/src/api/controllers/User/IUserController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+
+export interface IUserController {
+  create: (req: Request, res: Response) => Promise<void>
+  singIn: (req: Request, res: Response) => Promise<void>
+}
---
+++ app/backend/src/api/controllers/User/index.ts
@@ -0,0 +1,38 @@
+import { USER_SERVICE } from '@api/services/User';
+import { IUserService } from '@api/services/User/IUserService';
+import { UserType } from '@app/entities/User';
+import { Container, Inject, Injectable, InjectionToken } from '@decorators/di';
+import { Request, Response } from 'express';
+import { StatusCodes as status } from 'http-status-codes';
+import { IUserController } from './IUserController';
+
+export const USER_CONTROLLER = new InjectionToken('USER_CONTROLLER');
+
+@Injectable()
+export class UserController implements IUserController {
+  constructor(
+    @Inject(USER_SERVICE) private readonly service: IUserService
+  ) { }
+
+  async create(req: Request, res: Response) {
+    try {
+      const { name, email, password } = req.body as UserType;
+      const user = await this.service.create({ name, email, password } as UserType);
+      res.status(status.CREATED).json(user);
+    } catch (error) {
+      res.status(status.BAD_REQUEST).json({ error: (error as Error).message });
+    }
+  }
+
+  async singIn(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body as UserType;
+      const token = await this.service.singIn({ email, password });
+      res.status(status.OK).json({ token });
+    } catch (error) {
+      res.status(status.UNAUTHORIZED).json({ error: (error as Error).message });
+    }
+  }
+}
+
+Container.provide([{ provide: USER_CONTROLLER, useClass: UserController }]);
