526c6c1c3bdb1710460d065dd18296de23e87ede
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-11-21 00:54:12 -0300

    feat: creates User services.

---
+++ app/backend/src/api/services/User/IUserService.ts
@@ -0,0 +1,16 @@
+import { User, UserType } from '@app/entities/User';
+
+export type CreateUserRequest = UserType;
+export type CreateUserResponse = User;
+
+export interface SingInRequest {
+  email: string
+  password: string
+};
+
+export type SingInResponse = string;
+
+export interface IUserService {
+  create: (data: CreateUserRequest) => Promise<CreateUserResponse>
+  singIn: (data: SingInRequest) => Promise<SingInResponse>
+}
---
+++ app/backend/src/api/services/User/index.ts
@@ -0,0 +1,37 @@
+import { Container, Inject, Injectable, InjectionToken } from '@decorators/di';
+import { compareSync, hash } from 'bcrypt';
+import 'dotenv/config';
+import Jwt from 'jsonwebtoken';
+import { CreateUserRequest, CreateUserResponse, IUserService, SingInRequest, SingInResponse } from './IUserService';
+import { IUserServiceProps } from './IUserServiceProps';
+import { USER_SERVICE_PROPS } from './UseServicesProps';
+
+export const USER_SERVICE = new InjectionToken('USER_SERVICE');
+
+@Injectable()
+export class UserService implements IUserService {
+  constructor(
+    @Inject(USER_SERVICE_PROPS) private readonly useCases: IUserServiceProps
+  ) { }
+
+  async create(data: CreateUserRequest): Promise<CreateUserResponse> {
+    const password = await hash(data.password, 10);
+
+    const user = await this.useCases.create.execute({ ...data, password });
+    return user;
+  }
+
+  async singIn(data: SingInRequest): Promise<SingInResponse> {
+    const user = await this.useCases.findByEmail.execute(data.email);
+
+    const checkPass = compareSync(data.password, user.password);
+
+    if (!checkPass) throw new Error('Invalid password.');
+
+    const token = Jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '1d' });
+
+    return token;
+  }
+}
+
+Container.provide([{ provide: USER_SERVICE, useClass: UserService }]);
