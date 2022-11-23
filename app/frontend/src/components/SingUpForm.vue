31d1ae2a0fe5437ec730e33d20e155417e76716b
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-11-23 02:32:01 -0300

    feat: builds the sing up page.

---
+++ app/frontend/src/components/SingUpForm.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import HidePass from '@/assets/hidePass.svg?component';
+import ShowPass from '@/assets/showPass.svg?component';
+import { ref } from 'vue';
+const showPass = ref(false);
+const toggleShowPass = () => {
+  showPass.value = !showPass.value;
+};
+</script>
+<template>
+  <form class="flex items-center flex-col gap-4 justify-between px-4 mt-24">
+    <span class="self-start dark:text-gray-100">Faça o seu cadastro:</span>
+    <div
+      class="flex items-center bg-gray-100 dark:bg-gray-900 px-2 py-1 border-l border-green-400 rounded-sm w-full"
+    >
+      <input
+        type="text"
+        class="bg-gray-100 dark:bg-gray-900 placeholder-gray-900 p-2 dark:placeholder-gray-100 text-gray-900 dark:text-gray-100 font-light focus:outline-none block w-full appearance-none leading-normal"
+        placeholder="Qual o seu nome?"
+      />
+    </div>
+    <div
+      class="flex items-center bg-gray-100 dark:bg-gray-900 px-2 py-1 border-l border-green-400 rounded-sm w-full"
+    >
+      <input
+        type="email"
+        class="bg-gray-100 dark:bg-gray-900 placeholder-gray-900 p-2 dark:placeholder-gray-100 text-gray-900 dark:text-gray-100 font-light focus:outline-none block w-full appearance-none leading-normal"
+        placeholder="Digite o seu e-mail"
+      />
+    </div>
+    <div
+      class="flex items-center bg-gray-100 dark:bg-gray-900 px-2 py-1 border-l border-green-400 rounded-sm w-full"
+    >
+      <input
+        :type="showPass ? 'text' : 'password'"
+        class="bg-gray-100 dark:bg-gray-900 placeholder-gray-900 p-2 dark:placeholder-gray-100 text-gray-900 dark:text-gray-100 font-light focus:outline-none block w-full appearance-none leading-normal"
+        placeholder="Digite uma senha"
+      />
+      <button
+        @click="toggleShowPass"
+        type="button"
+        class="w-6 h-6 flex items-center justify-center"
+      >
+        <ShowPass v-if="showPass" class="fill-gray-900 dark:fill-gray-100" />
+        <HidePass v-else class="fill-gray-900 dark:fill-gray-100" />
+      </button>
+    </div>
+    <div class="flex w-full items-end justify-between">
+      <button
+        type="submit"
+        class="bg-green-400 hover:bg-green-500 text-gray-100 font-bold py-2 px-8 rounded-md"
+      >
+        Cadastrar
+      </button>
+    </div>
+  </form>
+</template>
--- app/frontend/src/views/SingUpView.vue
+++ app/frontend/src/views/SingUpView.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SingUpForm from '@/components/SingUpForm.vue';
+</script>
 <template>
-  <div></div>
+  <SingUpForm />
 </template>
