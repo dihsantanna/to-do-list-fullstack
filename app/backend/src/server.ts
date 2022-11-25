4157036d17aef8ace92eb0d933593d2c4f4de52a
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-11-25 00:09:42 -0300

    chore: configure backend app to deploy in vercel.

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,7 +1,13 @@
-
 import 'dotenv/config';
+import path from 'path';
 import { app } from './app';
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server is running! Port: ${PORT!}`));
+
+app.get('/', (_req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
+
+module.exports = app;
